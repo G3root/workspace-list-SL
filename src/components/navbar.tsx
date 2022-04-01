df09d2f6bd4b3ed192191bb9e5870f2622611f97
nafees nazik <nafeesnazik10@gmail.com>
2022-04-01 16:02:05 +0530

    add links for data source

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,13 +4,25 @@ import {
   IconButton,
   useColorModeValue,
   useColorMode,
-  Text,
+  Link,
   Heading,
+  HStack,
+  Icon,
+  Button,
 } from "@chakra-ui/react";
 import * as React from "react";
 import { FiMoon, FiSun } from "react-icons/fi";
 
 export interface NavbarProps {}
+
+const GithubIcon = (props: React.ComponentProps<"svg">) => (
+  <svg viewBox="0 0 20 20" {...props}>
+    <path
+      fill="currentColor"
+      d="M10 0a10 10 0 0 0-3.16 19.49c.5.1.68-.22.68-.48l-.01-1.7c-2.78.6-3.37-1.34-3.37-1.34-.46-1.16-1.11-1.47-1.11-1.47-.9-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.9 1.52 2.34 1.08 2.91.83.1-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.94 0-1.1.39-1.99 1.03-2.69a3.6 3.6 0 0 1 .1-2.64s.84-.27 2.75 1.02a9.58 9.58 0 0 1 5 0c1.91-1.3 2.75-1.02 2.75-1.02.55 1.37.2 2.4.1 2.64.64.7 1.03 1.6 1.03 2.69 0 3.84-2.34 4.68-4.57 4.93.36.31.68.92.68 1.85l-.01 2.75c0 .26.18.58.69.48A10 10 0 0 0 10 0"
+    />
+  </svg>
+);
 
 export function Navbar(props: NavbarProps) {
   const { toggleColorMode: toggleMode } = useColorMode();
@@ -31,16 +43,43 @@ export function Navbar(props: NavbarProps) {
         </Heading>
       </Box>
       <Box>
-        <IconButton
-          size="md"
-          fontSize="lg"
-          aria-label={`Switch to ${text} mode`}
-          variant="outline"
-          color="current"
-          ml={{ base: "0", md: "3" }}
-          onClick={toggleMode}
-          icon={<SwitchIcon />}
-        />
+        <HStack>
+          <Button
+            as={"a"}
+            href={process.env.SPREADSHEET_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            size="sm"
+            colorScheme="teal"
+          >
+            Data Source
+          </Button>
+          <Box mr="2">
+            <Link
+              isExternal
+              aria-label="view source code"
+              href={"https://github.com/G3root/workspace-list-SL"}
+            >
+              <Icon
+                as={GithubIcon}
+                display="block"
+                transition="color 0.2s"
+                w="5"
+                h="5"
+              />
+            </Link>
+          </Box>
+
+          <IconButton
+            size="md"
+            fontSize="lg"
+            aria-label={`Switch to ${text} mode`}
+            variant="outline"
+            color="current"
+            onClick={toggleMode}
+            icon={<SwitchIcon />}
+          />
+        </HStack>
       </Box>
     </Flex>
   );
