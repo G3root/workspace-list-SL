4e7bb47db5e5f9d3aa1b117954d4d98bfc1a0a1e
nafees nazik <nafeesnazik10@gmail.com>
2022-04-02 07:07:28 +0530

    add github action cron for page revalidation

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -10,6 +10,8 @@ export default async function handler(
   }
 
   try {
+    console.log("successful");
+
     await res.unstable_revalidate("/");
     return res.json({ revalidated: true });
   } catch (err) {
