9373855c4cea85b329be72b377deecce0c14a8c0
nafees nazik <nafeesnazik10@gmail.com>
2022-04-02 06:36:17 +0530

    add regex to replace linebreaks in GOOGLE_PRIVATE_KEY environment variable

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export const getStaticProps = async () => {
   const doc = new GoogleSpreadsheet(process.env.SPREADSHEET_ID);
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   });
   await doc.loadInfo();
   const sheet = doc.sheetsByIndex[0];
